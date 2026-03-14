bdbfe947a76594c9164cfa5ef031298cd9499891
Alexandru Stingheriu <alexandru.stingheriu@student.usv.ro>
2026-03-15 00:40:53 +0200

    Adaugat clasa persoana

---
+++ Proiect-PIU/Persoana.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_PIU
+{
+    class Persoana
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public int Varsta { get; set; }
+        
+        public Persoana(string nume, string prenume, int varsta)
+        {
+            Nume = nume;
+            Prenume = prenume;
+            Varsta = varsta;
+        }
+        public virtual void Afisare()
+        {
+            Console.WriteLine("Nume: {Nume}");
+            Console.WriteLine("Prenume: {Prenume}");
+            Console.WriteLine("Varsta: {Varsta}");
+        }
+    }
+}
---
+++ Proiect-PIU/Sofer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_PIU
+{
+    class Sofer: Persoana
+    {
+        public int kmParcursi { get; set; }
+
+        public Sofer(string nume, string prenume, int varsta, float km):base(nume,prenume,varsta)
+        {
+            kmParcursi = km;
+        }
+        public override void Afisare()
+        {
+            Console.WriteLine("Nume sofer: {Nume}");
+            Console.WriteLine("Prenume sofer: {Prenume}");
+            Console.WriteLine("Varsta sofer: {Varsta}");
+        }
+    }
+}
